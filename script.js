444dfc8a9f93c274f64b9b558e39d70c51f9184c
pronamvip1998 <trannamhai.dn@gmail.com>
2019-12-19 10:25:51 +0700

    [Hai] BT 1 : HTML CSS JS

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ const mybutton = document.getElementById("scroll-on-top");
                     mybutton.classList.remove('visuallyhidden');
                 }, 10);
             }
-        }else {
+        } else {
             mybutton.classList.add('visuallyhidden');    
             mybutton.addEventListener('transitionend', function(e) {
                 mybutton.classList.add('hidden');
@@ -27,11 +27,11 @@ mybutton.addEventListener('click', function() {
 const btnMenu = document.getElementById('btnMenu');
 const listMenu = document.getElementById('nav-main');
 
-btnMenu.addEventListener('click', () => {
+btnMenu.addEventListener('click', function() {
     const style = listMenu.style.display;
     const styleBtn = btnMenu.style.transform;
     listMenu.style.display = style == 'block' ? 'none' : 'block';
-})
+});
 const linkWhatWeDo = document.getElementById("whatwedo-link");
 const listWedo = document.getElementById('list-we-do');
 const btn = document.getElementById('btnMenu');
@@ -40,7 +40,6 @@ linkWhatWeDo.addEventListener('click', function(evt) {
     const stylelist = listWedo.style.display;
     btnMenu.style.transform = '' ? 'rotate(-45deg)': 'rotate(0)';
     const styleBtnMenu = btn.style.transform;
-    console.log()
     listWedo.style.display = stylelist == 'block' ? 'none' : 'block';
     btn.style.transform = styleBtnMenu == 'rotate(-90deg)' ? 'rotate(-45deg)': 'rotete(-90deg)';
-},false)
+},false);
